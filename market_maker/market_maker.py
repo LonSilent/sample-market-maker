9bfb384b39cb91e0a01fab215953c7490cc6f591
rob0215 <101703044@nccu.edu.tw>
2017-08-18 05:10:11 +0800

    remove min max

--- market_maker/market_maker.py
+++ market_maker/market_maker.py
@@ -340,12 +340,12 @@ class OrderManager:
                 if sell_orders[i]['price'] < cost_position:
                     sell_orders[i]['price'] = round(cost_position + 10 + 20 * (len(sell_orders) - i), 1)
 
-        for i in reversed(range(len(buy_orders))):
-            if buy_orders[i]['price'] > settings.MAX_BUY_POSITION:
-                buy_orders[i]['price'] = round(settings.MAX_BUY_POSITION - 10 - 20 * (len(buy_orders) - i), 1)
-        for i in reversed(range(len(sell_orders))):
-            if sell_orders[i]['price'] < settings.MIN_SELL_POSITION:
-                sell_orders[i]['price'] = round(settings.MIN_SELL_POSITION + 10 + 20 * (len(sell_orders) - i), 1)
+        # for i in reversed(range(len(buy_orders))):
+        #     if buy_orders[i]['price'] > settings.MAX_BUY_POSITION:
+        #         buy_orders[i]['price'] = round(settings.MAX_BUY_POSITION - 10 - 20 * (len(buy_orders) - i), 1)
+        # for i in reversed(range(len(sell_orders))):
+        #     if sell_orders[i]['price'] < settings.MIN_SELL_POSITION:
+        #         sell_orders[i]['price'] = round(settings.MIN_SELL_POSITION + 10 + 20 * (len(sell_orders) - i), 1)
 
         return self.converge_orders(buy_orders, sell_orders)
 
